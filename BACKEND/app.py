a2cada1dbc698a7b400caa5b7a45ea5febd217d1
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2024-08-02 14:32:10 +0100

    Update app.py

--- BACKEND/app.py
+++ BACKEND/app.py
@@ -14,7 +14,7 @@ load_dotenv()
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
